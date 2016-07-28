6748f12643a4443397cb68a47728ddfe33db4c9f
Sven Wappler <info@wappler.systems>
2016-07-28 23:25:36 +0200

    Kompatibilität angepasst

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-7.9.99',
+			'typo3' => '7.6.0-7.9.99',
             'vhs' => '',
 		),
 		'conflicts' => array(
