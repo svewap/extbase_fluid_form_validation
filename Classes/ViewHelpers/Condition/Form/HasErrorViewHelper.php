fe99a1a5de368ed760b270c9b06e2e2d2d3273f4
Gregor Agnes <ga@zwo3.de>
2018-12-21 17:20:12 +0100

    Update class HasErrorViewHelper for TYPO3 9.5
    I don't know if it works with 8.7 to

--- Classes/ViewHelpers/Condition/Form/HasErrorViewHelper.php
+++ Classes/ViewHelpers/Condition/Form/HasErrorViewHelper.php
@@ -2,36 +2,43 @@
 namespace WapplerSystems\ExtbaseFluidFormValidation\ViewHelpers\Condition\Form;
 
 
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
 /**
  * Class HasErrorViewHelper
  *
- * Hätte diese gerne mit AbstractConditionViewHelper realisiert, aber da gibt es wegen der static Methoden Probleme auf den controllerContext zurückzugreifen
- *
- * @package WapplerSystems\ExtbaseFluidFormValidation\ViewHelpers\Condition\Form
+ * @package Zwo3\Klimatour\ViewHelpers\Form
  */
-class HasErrorViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
-{
+class HasErrorViewHelper extends AbstractViewHelper {
 
+    public function initializeArguments()
+    {
+        $this->registerArgument('for', 'string', 'The property', true);
+        $this->registerArgument('then', 'string', 'The class to add', true);
+    }
 
     /**
-     * @param string $for
-     * @param string $then
-     * @return string
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return mixed
      */
-    public function render($for = '', $then = '')
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
     {
-
-        $validationResults = $this->controllerContext->getRequest()->getOriginalRequestMappingResults();
+        $controllerContext = $renderingContext->getcontrollerContext();
+        $validationResults = $controllerContext->getRequest()->getOriginalRequestMappingResults();
+        $for = $arguments['for'];
+        $then = $arguments['then'];
 
         if ($validationResults !== null && $for !== '') {
             $validationResults = $validationResults->forProperty($for);
         }
-
         if ($validationResults->hasErrors()) {
             return $then;
         }
         return "";
     }
-
 
 }
